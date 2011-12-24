d5be3194de3c65ac3b52f5ee2fb96c4b1753d944
Erik Broes <erikbroes@grum.nl>
2011-12-24 16:48:23 +0100

    Add initial EventDispatcher

---
+++ src/main/java/nl/grum/microcraft/EventDispatch.java
@@ -0,0 +1,48 @@
+package nl.grum.microcraft;
+
+import java.util.List;
+import java.util.Map;
+
+import com.google.common.collect.Lists;
+import com.google.common.collect.Maps;
+
+public class EventDispatch {
+    @SuppressWarnings("rawtypes")
+    private final Map<Class, List> map = Maps.newHashMap();
+
+    public <L> void register(Class<? extends Event<L>> eventClass, L listener) {
+        List<L> listeners = listenersOf(eventClass);
+        synchronized (listener) {
+            listeners.add(listener);
+        }
+    }
+
+    public <L> void unregister(Class<? extends Event<L>> eventClass, L listener) {
+        List<L> listeners = listenersOf(eventClass);
+        synchronized (listener) {
+            listeners.remove(listener);
+        }
+    }
+
+    private <L> List<L> listenersOf(Class<? extends Event<L>> eventClass) {
+        synchronized (map) {
+            @SuppressWarnings("unchecked")
+            List<L> list = map.get(eventClass);
+            if (list == null) {
+                list = Lists.newArrayList();
+                map.put(eventClass, list);
+            }
+
+            return list;
+        }
+    }
+
+    public <L> void notify(final Event<L> event) {
+        @SuppressWarnings("unchecked")
+        Class<Event<L>> eventClass = (Class<Event<L>>) event.getClass();
+
+        for (L listener : listenersOf(eventClass)) {
+            event.notify(listener);
+        }
+    }
+}
